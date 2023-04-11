d767bd434d09bf51bd1849b2690c4fc4a14828c5
Xander Burger <xanburger@hotmail.com>
2023-04-11 16:34:13 -0600

    finished infographics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import SvgAnimation from "./SvgAnimation"
+
 import InfoGraphic from "./InfoGraphic";
 
 function App() {
+  document.cookie = "Tracker Cookie; expires=fri, 12 Apr 2024 00:00:00 GMT"
   return (
     <>
       <div className="main"> 
         <h1 className="tagLine">
         HAVE A COOKIE
         </h1>
-        {/* <SvgAnimation/>   */}
         <InfoGraphic/>
       </div>
 
--- src/InfoGraphic.jsx
+++ src/InfoGraphic.jsx
@@ -7,25 +7,25 @@ export default function InfoGraphic() {
   return (
     <div className='infoGraphic' >
         <p>
-            <b>How cookies work:</b>
+            <b>How Cookies Track You</b>
         </p>
     <div className='graphicsContainer'>
         <div className='graphic1'>
             <img src={graphic1} alt="website SVG" />
             <p>
-                content description
+            Tracking cookies are added to your browsers from content hosted by 3rd parties like Facebook or Google.
             </p>
         </div>
         <div className='graphic2'>
             <img src={graphic2} alt="sever SVG" />
             <p>
-                content description
+            These cookies are stored locally on your browser, keeping track of your browsing activity. This activity is sent to the 3rd party's servers and stored in a profile under your IP address.
             </p>
         </div>
         <div className='graphic3'>
             <img src={graphic3} alt="brand website SVG" />
             <p>
-                content description
+            Businesses then pay companies like Google Ads or Facebook Ads to send ads to profiles that match their desired demographics.
             </p>
         </div>
 
